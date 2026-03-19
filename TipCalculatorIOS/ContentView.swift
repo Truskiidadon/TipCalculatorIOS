46e91ffb464dcfa9d9b850ba2394a1c0825f4e1f
Alchemist <141343701+Truskiidadon@users.noreply.github.com>
2026-03-19 03:33:43 -0400

    TipCalculator using SwiftUI syntax

--- TipCalculatorIOS/ContentView.swift
+++ TipCalculatorIOS/ContentView.swift
@@ -8,14 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    
+    @State private var billAmount: String = ""
+    @State private var resultText: String = ""
+    
+    var body: some View{
+        VStack(spacing: 24){
+            
+            Text("Tip Calculator")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+            TextField("Enter bill amount here", text: $billAmount)
+                .textFieldStyle(.roundedBorder)
+                .keyboardType(.decimalPad)
+                .frame(maxWidth: 300)
+            
+            HStack(spacing: 16){
+                
+            }
         }
-        .padding()
     }
 }
 
