84e4ebda9bc26b44b4382c36dccaae3dcdc3d38c
Alchemist <141343701+Truskiidadon@users.noreply.github.com>
2026-03-19 03:23:29 -0400

    Initial Commit

---
+++ TipCalculatorIOS/TipCalculatorIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorIOSApp.swift
+//  TipCalculatorIOS
+//
+//  Created by Elliott Vaughn-Truesdale on 3/19/26.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
